c7dab01d312fd719a576a1a1f0c7bbdb698477a0
Ketan Kumbhar <ketankumbhar51@gmail.com>
2026-06-06 22:07:07 +0530

    Initial React Native setup

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to CountIn</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
